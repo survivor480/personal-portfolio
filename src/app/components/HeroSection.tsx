220680d728e2d3d6af1710d25951a5e620381873
Keshav <keshav@distronix.in>
2025-04-11 01:06:01 +0530

    Made changes to favicon and some icons

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -64,7 +64,6 @@ export default function HeroSection() {
                         repeat={Infinity}
                         style={{ display: 'inline-block' }}
                         cursor
-                        cursorStyle="|"
                     />
                 </motion.div>
 
